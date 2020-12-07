79cac6e14042a32e54a16a56522f17d37a3ea142
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-07 05:43:48 +0200

    feat: shared module was added with car and user interfaces

---
+++ real-exam/src/app/shared/interfaces/car.ts
@@ -0,0 +1,12 @@
+export interface ICar {
+  _id: string;
+  modelName: string;
+  description: string;
+  price: number;
+  contact: string;
+  imageUrl: string;
+  author: string;
+  created_at: string;
+  updatedAt: string;
+  __V: number;
+}

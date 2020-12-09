0b20eabb86f76805a44da05f58b4ed6a5bc0eb22
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-09 20:19:53 +0200

    feat: css was added in some files, home rout, and some more changes

--- real-exam/src/app/user/user.module.ts
+++ real-exam/src/app/user/user.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserService } from './user.service';
 
 
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    UserService
   ]
 })
 export class UserModule { }

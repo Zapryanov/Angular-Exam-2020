12dd4bc0d86e357e9b41feb67ec45c8f8acd4c34
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-11 11:01:51 +0200

    feat: contact page was created

--- real-exam/src/app/app-routing.module.ts
+++ real-exam/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 // import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: "about",
     component: AboutComponent
+  },
+  {
+    path: "contact",
+    component: ContactComponent
   },
   {
     path: "**",
--- real-exam/src/app/app.module.ts
+++ real-exam/src/app/app.module.ts
@@ -10,13 +10,15 @@ import { UserModule } from './user/user.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NotFoundComponent,
-    AboutComponent
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
